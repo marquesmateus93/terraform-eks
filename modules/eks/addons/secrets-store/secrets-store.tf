724eac919cb269404210cbf3fcdc73d50bbd4b67
Mateus Marques <mateusmarques1993@gmail.com>
2023-07-05 09:45:59 -0300

    Secrets Store.

--- modules/eks/addons/secrets-store/secrets-store.tf
+++ modules/eks/addons/secrets-store/secrets-store.tf
@@ -3,4 +3,14 @@ resource "helm_release" "secrets-store" {
   repository  = var.helm.repository
   chart       = var.helm.chart
   namespace   = var.helm.namespace
+
+  set {
+    name  = "image.tag"
+    value = "1.0.r2-46-gf2a8f35-2023.03.21.21.55"
+  }
+
+  set {
+    name  = "grpcSupportedProviders"
+    value = "aws"
+  }
 }
